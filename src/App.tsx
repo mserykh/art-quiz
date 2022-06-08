ea5bdcc5d0bc04dfe41f53eb35b378e81d8f6499
Maria Serykh <marie.serykh@gmail.com>
2022-06-08 20:42:34 +0200

    init: create project and configure it

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.scss';
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
